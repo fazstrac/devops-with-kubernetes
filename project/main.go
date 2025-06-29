4c19372a8825c0d264dd084ce254b23822830e28
Sami Kujala <skujala@iki.fi>
2025-06-29 13:28:17 +0300

    First try on project build

---
+++ project/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		os.Setenv("PORT", port)
+	}
+
+	router := setupRouter()
+	router.Run("0.0.0.0:" + port)
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.GET("/", getIndex)
+	// Add more routes here as needed
+	return router
+}
+
+func getIndex(c *gin.Context) {
+	c.String(http.StatusOK, "Server started in port %s", os.Getenv("PORT"))
+}
