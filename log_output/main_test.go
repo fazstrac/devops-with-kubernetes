b751ba731112877149b967c8faccb58a80520567
Sami Kujala <skujala@iki.fi>
2025-07-06 19:34:27 +0300

    Refactor tests into more compact suite

--- log_output/main_test.go
+++ log_output/main_test.go
@@ -6,29 +6,28 @@ import (
 	"os"
 	"testing"
 
+	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 )
 
+var testRouter *gin.Engine
+
+func TestMain(m *testing.M) {
+	os.Setenv("PORT", "8080")
+	testRouter = setupRouter("TestMain")
+	os.Exit(m.Run())
+}
+
 func TestGetIndex(t *testing.T) {
-	os.Setenv("PORT", "8080") // Ensure PORT is set for the handler
-
-	router := setupRouter("TestGetIndex") // Use the same router logic as in main.go
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/", nil)
-	router.ServeHTTP(w, req)
-
+	testRouter.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusMovedPermanently, w.Code)
 }
 
 func TestGetLog(t *testing.T) {
-	os.Setenv("PORT", "8080") // Ensure PORT is set for the handler
-
-	router := setupRouter("TestGetLog") // Use the same router logic as in main.go
-
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/log", nil)
-	router.ServeHTTP(w, req)
-
+	testRouter.ServeHTTP(w, req)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
