14d15ab35e8feb86b2677e8379bfe81a577acdbf
Sami Kujala <skujala@iki.fi>
2025-07-07 15:30:00 +0300

    Reorganize CI and begin pong_app

---
+++ pong_app/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+
+	"github.com/gin-gonic/gin"
+)
+
+var (
+	// COMMIT_SHA and COMMIT_TAG are set by the build system
+	COMMIT_SHA   string
+	COMMIT_TAG   string
+	counter      int
+	counterMutex sync.Mutex // Mutex to protect counter access
+)
+
+func main() {
+	counter = 0 // Initialize counter
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		os.Setenv("PORT", port)
+	}
+
+	router := setupRouter()
+	router.Run("0.0.0.0:" + port)
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, incrCounter())
+	})
+	return router
+}
+
+func incrCounter() string {
+	counterMutex.Lock()
+	counter++
+	value := strconv.Itoa(counter)
+	counterMutex.Unlock()
+
+	return "pong " + value
+}
---
+++ pong_app/main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+var testRouter *gin.Engine
+
+func TestMain(m *testing.M) {
+	os.Setenv("PORT", "8080")
+	testRouter = setupRouter()
+	os.Exit(m.Run())
+}
+
+func TestIncrCounter(t *testing.T) {
+	counterMutex.Lock()
+	counter = 0
+	counterMutex.Unlock()
+	expected := "pong 1"
+	result := incrCounter()
+	assert.Equal(t, expected, result)
+}
+
+func TestIncrCounterEndpoint(t *testing.T) {
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("GET", "/", nil)
+	testRouter.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
