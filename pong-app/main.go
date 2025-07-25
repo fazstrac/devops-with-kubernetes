38140fcc3050813f88c689dd2033af18125c6545
Sami Kujala <skujala@iki.fi>
2025-07-26 01:27:49 +0300

    Commit tonight's unfinished work [no-ci]

--- pong-app/main.go
+++ pong-app/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 	"strconv"
@@ -15,10 +16,25 @@ var (
 	COMMIT_TAG   string
 	counter      int
 	counterMutex sync.Mutex // Mutex to protect counter access
+	fname        string
 )
 
 func main() {
 	counter = 0 // Initialize counter
+
+	arglen := len(os.Args[1:])
+
+	if arglen == 0 {
+		fmt.Printf("Usage: %s <filename>\n", os.Args[0])
+		os.Exit(1)
+	} else if arglen > 1 {
+		fmt.Printf("Please give only one filename. Usage: %s <filename>\n", os.Args[0])
+		os.Exit(1)
+	}
+
+	fname = os.Args[1]
+
+	fmt.Printf("Starting pong-app (SHA %s).\n", COMMIT_SHA)
 
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -43,6 +59,33 @@ func incrCounter() string {
 	counterMutex.Lock()
 	counter++
 	value := strconv.Itoa(counter)
+
+	// The purpose is to pass the counter value via filesystem
+	// to other pods sharing the same file system.
+	// This may just work, but a major issue is that
+	// there is no file locking and sharing data like this is
+	// JUST DEAD WRONG to begin with.
+	//
+	// In production one should use something like
+	// Valkey / Redis, RabbitMQ, NATS to share
+	// the counter value.
+
+	func() {
+		fp, err := os.Create(fname)
+
+		if err != nil {
+			// something went really wrong, die as it's can't expect it to be a transient issue
+			panic(err)
+		}
+		defer fp.Close()
+
+		_, err = fp.WriteString(value)
+
+		if err != nil {
+			// Writing should not fail, so die screaming if there's an error
+			panic(err)
+		}
+	}()
 	counterMutex.Unlock()
 
 	return "pong " + value
